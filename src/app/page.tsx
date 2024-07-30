00e4e02a1df937c147a90983756e8d22a4259b24
Steve Krenn <stevekrenn@gmail.com>
2024-07-30 12:49:33 -0500

    add snippet render to page - frame out page id dynamic route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Home Page</h1>
-    </>
-  );
+import { db } from '@/db';
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippets) => {
+    return (
+      <div key={snippets.id}>
+        {snippets.title} {snippets.code}
+      </div>
+    );
+  });
+
+  return <div>{renderedSnippets}</div>;
 }

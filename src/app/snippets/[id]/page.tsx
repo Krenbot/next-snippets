0d6e4169696a588f2899c4e922aca07a5fda69aa
Steven Krenn <stevekrenn@gmail.com>
2024-08-23 15:08:12 -0500

    add delete action

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
-import Link from 'next/link';
+import * as actions from '@/actions';
 
 interface SnippetShowPageProps {
   params: {
@@ -9,8 +10,7 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
-  // Loading timeout debug
-  // await new Promise((r) => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
 
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(props.params.id) },
@@ -20,13 +20,10 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
   return (
-    <>
-      <div>
-        <Link className="text-xl font-bold" href={`/`}>
-          Home
-        </Link>
-      </div>
+    <div>
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
         <div className="flex gap-4">
@@ -36,12 +33,14 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
           >
             Edit
           </Link>
-          <button className="p-2 border rounded">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
         <code>{snippet.code}</code>
       </pre>
-    </>
+    </div>
   );
 }
